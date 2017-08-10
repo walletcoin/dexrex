9f89b6dae9efa1edbaf8cf281b8f2bf821b0d104
decentrex <decentrex@protonmail.com>
2017-08-10 15:28:00 -0400

    update

--- js/bundle.js
+++ js/bundle.js
@@ -2235,7 +2235,7 @@ function candlestickChart(elem, title, xtitle, ytitle, data, minValue, maxValue)
           width: $(`#${elem}`).parent().width(),
           height: $(`#${elem}`).parent().height(),
           chartArea: { left: 50, width: '90%', height: '80%' },
-          backgroundColor: { fill: 'transparent' },
+          backgroundColor: { color: '#fff', fill: 'transparent' },
           colors: ['#ccc'],
           hAxis: {
             title: xtitle,
@@ -2281,7 +2281,7 @@ function depthChart(elem, title, xtitle, ytitle, data, minX, maxX) {
           width: $(`#${elem}`).parent().width(),
           height: $(`#${elem}`).parent().height(),
           chartArea: { left: 50, width: '90%', height: '80%' },
-          backgroundColor: { fill: 'transparent' },
+          backgroundColor: { color: '#fff', fill: 'transparent' },
           colors: ['#d9534f', '#00796b'],
           title,
           hAxis: {
